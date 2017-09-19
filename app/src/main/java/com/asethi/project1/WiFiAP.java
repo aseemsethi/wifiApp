be50d2ef6fe57726c806f96e1db516a66d2a55d0
aseemsethi <aseemsethi@yahoo.com>
2017-09-19 13:30:56 +0530

    Added Connect and disconect to wifi

--- app/src/main/java/com/asethi/project1/WiFiAP.java
+++ app/src/main/java/com/asethi/project1/WiFiAP.java
@@ -1,7 +1,12 @@
 package com.asethi.project1;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,6 +15,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,6 +26,9 @@ public class WiFiAP extends AppCompatActivity {
     WiFiProperties wifiP;
     String ssid, bssid, level, cap;
     TextView ssidUI, bssidUI, capUI, levelUI;
+    EditText passw;
+    WifiManager wifiManager;
+    BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,16 +69,57 @@ public class WiFiAP extends AppCompatActivity {
                         InputMethodManager.HIDE_NOT_ALWAYS);
             }
         });
-        Button conn = (Button) findViewById(R.id.connect);
+        final Button conn = (Button) findViewById(R.id.connect);
         conn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                passw = (EditText) findViewById(R.id.pwd);
                 InputMethodManager inputManager = (InputMethodManager)
                         getSystemService(Context.INPUT_METHOD_SERVICE);
                 inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
                         InputMethodManager.HIDE_NOT_ALWAYS);
+
+                broadcastReceiver = new WifiReceiver();
+                IntentFilter intentFilter = new IntentFilter();
+                intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+                registerReceiver(broadcastReceiver, intentFilter);
+                WifiConfiguration wifiConfig = new WifiConfiguration();
+                wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+                if (conn.getText() == "Disconnect") {
+                    wifiManager.disconnect();
+                    System.out.println("Aseem: WiFiAP Disconnecting");
+                    conn.setText("Connect");
+                    return;
+                }
+                wifiConfig.SSID = String.format("\"%s\"", ssid);
+                wifiConfig.preSharedKey = String.format("\"%s\"", passw.getText());
+                int netId = wifiManager.addNetwork(wifiConfig);
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(netId, true);
+                wifiManager.reconnect();
             }
         });
+    }
+    public class WifiReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Button cd = (Button) findViewById(R.id.connect);
+            final String action = intent.getAction();
+            if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                boolean connected = info.isConnected();
+                if (connected) {
+                    // wifi connected
+                    System.out.println("Aseem: WiFiAP Connected");
+                    cd.setText("Disconnect");
+                } else {
+                    // wifi connection was lost
+                    System.out.println("Aseem: WiFiAP Disconnected");
+                    cd.setText("Connect");
+                }
+            }
+        }
     }
 
 }
